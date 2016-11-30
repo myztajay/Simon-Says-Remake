39dec0a930b485c48eabc3e64e0c5a32e8b9921b
myztajay <myztajay@hotmail.com>
2016-11-30 13:03:26 -0500

    Linked my JS and CSS. Made mockup of visual components of simon game

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("js in connected");
+
+//vaiables - store all my simon buttoms + play button
+
+//FOR VARIABLE ONCLICK
+
+//ARRAYS
+//Array to hold simonsequence
+//Array to hold usersequence
+
+//FUNCTIONS
+// functio for button clicked using this to refer to button tht clicked
+//find its ID using jquery
+// store array
