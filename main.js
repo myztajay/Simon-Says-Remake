1fde62708f2125db42521ba9f0644f1c914d2a4a
myztajay <myztajay@hotmail.com>
2016-11-30 22:45:54 -0500

    Added storeUserseq function converts there pushes to number into an array

--- main.js
+++ main.js
@@ -1,56 +1,89 @@
 console.log("js in connected");
-
-//vaiables - store all my simon buttoms + play button
-$red = $(btnRed);
-$blue = $(btnBlue);
-$green = $(btnGreen);
-$yellow = $(btnYellow);
-$play = $(btnPlay);
-
-//FOR VARIABLE ONCLIgreen
-$red.on('click')
-$blue.on('click')
-$green.on('click')
-$yellow.on('click')
-$play.on('click', playSimon)
-
+//JQUERY - VARIABLES
+let $red = $(btnRed);
+let $blue = $(btnBlue);
+let $green = $(btnGreen);
+let $yellow = $(btnYellow);
+let $play = $(btnPlay);
+let playerTurn = true;
+let tempVar
 
 //ARRAYS
-//Array to hold simonsequence
-//Array to hold usersequence
 let userSeq = [];
 let simonSeq = [];
 
+//VARIABLES CLICK EVENTS
+$red.on('click',storeUserSequence);
+$blue.on('click',storeUserSequence);
+$green.on('click', storeUserSequence);
+$yellow.on('click', storeUserSequence);
+$play.on('click', false);
 
 //FUNCTIONS
 // functio for button clicked using this to refer to button tht clicked
 //find its ID using jquery
 //  store array
-
-
-// generate random number from 1-4
+function storeUserSequence(){
+  console.log(this.id + "button was pressed");
+  if (this.id == "btnRed"){
+    $red.html("red_User");
+    tempVar = 1;
+    userSeq.push(tempVar);
+  }
+  if (this.id == "btnBlue"){
+    $blue.html("blue_User");
+    tempVar = 2;
+    userSeq.push(tempVar);
+  }
+  if (this.id == "btnGreen"){
+    $green.html("green_User");
+    tempVar = 3;
+    userSeq.push(tempVar);
+  }
+  else if (this.id == "btnYellow"){
+    $yellow.html("yellow_User");
+    tempVar = 4;
+    userSeq.push(tempVar);
+  }
+}
 
 //initiates game
-function playSimon() {
-    for (let i = 0; i < 3; i++) {
-        randomSeq = Math.floor((Math.random() * 4) + 1);
-        simonSeq.push(randomSeq);
-        console.log(simonSeq);
-    }
+function genSimonSeq(){
+    // check to see if playerTurn is true or false
+    if (playerTurn) {
+        for (let i = 0; i < 3; i++) {
+            randomSeq = Math.floor((Math.random() * 4) + 1);
+            simonSeq.push(randomSeq);
+            console.log(simonSeq);
+        }
+        for (let j in simonSeq) {
 
-    for (let j in simonSeq) {
-        if (simonSeq[j] == 1) {
-            $red.html("red");
+            if (simonSeq[j] == 1) {
+                //clear interval
+                $red.html("red");
+                //fill in with ccs3 feature highlight
+                //set invterval to freeze it
+            }
+            if (simonSeq[j] == 2) {
+                $blue.html("blue");
+                //fill in with ccs3 feature highlight
+                //set invterval to freeze it
+            }
+            if (simonSeq[j] == 3) {
+                $green.html("green");
+                //fill in with ccs3 feature highlight
+                //set invterval to freeze it
+            }
+            if (simonSeq[j] == 4) {
+                $yellow.html("yellow");
+                //fill in with ccs3 feature highlight
+                //set invterval to freeze it
+            }
         }
-        if (simonSeq[j]  == 2){
-            $blue.html("blue");
-        }
-        if (simonSeq[j] == 3) {
-            $green.html("green");
-        }
-        if (simonSeq[j] == 4) {
-            $yellow.html("yellow");
-        }
-    }
+    } else {
+      // Now we do what playas do
+        console.log("player turn now")
 
+    }
+    playerTurn = true;
 }
