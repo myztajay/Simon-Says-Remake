5ba6bccf760df8a75cfb3d758f29a48e670fe7e6
myztajay <myztajay@hotmail.com>
2016-12-02 23:19:51 -0500

    refactored code, still running into bug with round not ending

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ let $blue = $(btnBlue);
 let $green = $(btnGreen);
 let $yellow = $(btnYellow);
 let $play = $(btnPlay);
-let playerTurn = true;
-let tempVar;
+//REGULAR VARIABLES
+let ColorNum;
 let arrayCounter = 0;
-let simonCounter = 0
+let simonCounter = 0;
+let round =1;
 let timeoutId;
 var animate;
 
@@ -37,11 +38,12 @@ function resetUserSeq() {
 //RESET game
 function resetSimon(){
   simonSeq = [];
+  round = 1;
+  $('span').html('<H2>LOST</H2>');
 }
 //SIMON FLASHING FUNCTION
 function flash() {
     if (simonCounter < simonSeq.length) {
-        console.log("animate test");
         x = simonSeq[simonCounter];
         if (x == 1) {
             highlight($red);
@@ -56,7 +58,7 @@ function flash() {
             highlight($yellow);
         }
         simonCounter++;
-        console.log(simonCounter)
+        console.log(simonCounter+" simonmini counter")
     }
 
     function highlight(colorVar) {
@@ -82,67 +84,71 @@ function userFlash(colorVar) {
 //PUSHES USER VALUES TO ARRAY, USERFLASH
 function storeUserSequence() {
     console.log(this.id + "button was pressed");
-    if (arrayCounter < (simonSeq.length - 1)) {
-
+    if (arrayCounter != (simonSeq.length)) {
         if (this.id == "btnRed") {
-            userFlash($red)
-            tempVar = 1;
-            userSeq.push(tempVar);
+            userFlash($red);
+            ColorNum = 1;
+            userSeq.push(ColorNum);
 
             if (userSeq[arrayCounter] === simonSeq[arrayCounter]) {
                 arrayCounter++;
             } else {
                 // end the game if answer is wrong
-                alert('lost')
+                alert('lost');
                 console.log("Loo-hoo-looha");
                 resetUserSeq();
                 resetSimon();
             }
         }
         if (this.id == "btnBlue") {
-            userFlash($blue)
-            tempVar = 2;
-            userSeq.push(tempVar);
+            userFlash($blue);
+            ColorNum = 2;
+            userSeq.push(ColorNum);
 
             if (userSeq[arrayCounter] === simonSeq[arrayCounter]) {
                 arrayCounter++;
+                console.log(arrayCounter + " array counter");
+                console.log(userSeq[arrayCounter] + " user seq index of arrayCounter");
+                console.log(simonSeq[arrayCounter]+ " user seq index of arrayCounter");
             } else {
                 // end the game if answer is wrong
-                alert('lost')
+                alert('lost');
                 console.log("Loo-hoo-looha");
                 resetUserSeq();
                 resetSimon();
             }
         }
         if (this.id == "btnGreen") {
-            userFlash($green)
-            tempVar = 3;
-            userSeq.push(tempVar);
+            userFlash($green);
+            ColorNum = 3;
+            userSeq.push(ColorNum);
             if (userSeq[arrayCounter] === simonSeq[arrayCounter]) {
                 arrayCounter++;
             } else {
                 // end the game if answer is wrong
-                alert('lost')
+                alert('lost');
                 console.log("Loo-hoo-looha");
-                resetUserSeq();
-                resetSimon();
-            }
-        } else if (this.id == "btnYellow") {
-            userFlash($yellow)
-            tempVar = 4;
-            userSeq.push(tempVar);
-            if (userSeq[arrayCounter] === simonSeq[arrayCounter]) {
-                arrayCounter++;
-            } else {
-                // end the game if answer is wrong
-                console.log("Loo-hoo-looha");
-                alert('lost')
                 resetUserSeq();
                 resetSimon();
             }
         }
-    } else {
-        alert('winner')
+        if (this.id == "btnYellow") {
+            userFlash($yellow);
+            ColorNum = 4;
+            userSeq.push(ColorNum);
+            if (userSeq[arrayCounter] === simonSeq[arrayCounter]) {
+                arrayCounter++;
+            } else {
+                // end the game if answer is wrong
+                console.log("Loo-hoo-looha");
+                alert('lost');
+                resetUserSeq();
+                resetSimon();
+            }
+        }
+    } // end  of if stat
+    else {
+        alert('winner');
         console.log("YOU WIN BITCH!");
         resetUserSeq();
         genSimonSeq();
@@ -150,14 +156,15 @@ function storeUserSequence() {
 }
 //INIATES SIMON
 function genSimonSeq() {
+    $('span').html('<h2> round '+ round +'</h2>')
     randNum = Math.floor((Math.random() * 4) + 1);
     simonSeq.push(randNum);
-    console.log(simonSeq);
+    console.log(simonSeq + " = this is simonseq");
     // for (let i = 0; i < 3; i++) {
     //     randomSeq = Math.floor((Math.random() * 4) + 1);
     //     simonSeq.push(randomSeq);
     //     console.log(simonSeq);
     // }
-
     animate = setInterval(flash, 1000);
+    round++;
 }
